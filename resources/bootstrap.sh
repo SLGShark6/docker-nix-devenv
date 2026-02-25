1959f2f221a66c6d45cc7b91b761eb4e76de6c27
Shane Gardner <35613914+SLGShark6@users.noreply.github.com>
2026-02-25 17:25:10 -0500

    Feature/add bash (#8)
    * Update bootstrap.sh
    
    Add Bash to installed nix packages and set Bash as the default shell for the created dev user
    
    * Add Bash profile file to the dev user
    
    - Created `.bashrc` bash profile file, that includes the direnv bash hook and also includes the `.shellinit` file
    - In Dockerfile create ENV variable for the name of the Bash profile file
    - In the bootstrap script, move the bash profile to the dev user home directory
    
    * Update meta.json
    
    Increment minor version from `3.1.0` to `3.2.0`

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -15,13 +15,21 @@ mv $RESOURCES_DIR/$NIX_CONFIG_FILE $NIX_CONFIG_PATH
 nix profile add \
     nixpkgs#devenv \
     nixpkgs#direnv \
+    nixpkgs#bash \
     --profile /nix/var/nix/profiles/default
 
 # Add a non-root group & user
 addgroup $DEV_USER
-adduser $DEV_USER --disabled-password -G $DEV_USER
+adduser $DEV_USER --disabled-password -G $DEV_USER -s $(which bash)
 # Ensure the user is a part of the users that can use nix
 addgroup $DEV_USER nix
+
+# Get the DEV_USER home directory
+dev_home=$(getent passwd $DEV_USER | cut -d: -f6)
+# Move the bash profile to the dev user's directory
+mv $RESOURCES_DIR/$BASH_PROFILE_FILE $dev_home/$BASH_PROFILE_FILE
+# Ensure the user owns it
+chown $DEV_USER:$DEV_USER $dev_home/$BASH_PROFILE_FILE
 
 # Make a workspace directory to copy a project to
 mkdir $WORKING_DIR
